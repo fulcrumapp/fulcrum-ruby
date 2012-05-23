8122c61ae9b87480210975620c9c821699f16136
Chris Hall <chris@spatialnetworks.com>
2012-05-23 13:37:54 -0400

    some changes

--- lib/fulcrum/api.rb
+++ lib/fulcrum/api.rb
@@ -9,24 +9,26 @@ module Fulcrum
   
   class Api
     
-    @@key = nil
     @@uri = 'http://localhost:3000/api/v2'
-    @connection = nil
     
-    def self.key=(k)
-      @@key = k
-    end
+    attr :connection
     
-    def self.key
-      @@key
-    end
-    
-    def self.connection
-      raise ApiError, 'API Key not set' if !@@key
-      @@connection ||= Faraday.new(@@uri,
-                                headers: { 'X-ApiToken' => @@key,
-                                           :user_agent => "Ruby Fulcrum API Client version #{Fulcrum::VERSION}",
-                                           :accept => 'application/json' }) do |f|
+    def initialize(opts = {})
+      if opts[:username] && opts[:password]
+        connection = Faraday.new(@@uri, headers: {  user_agent: "Ruby Fulcrum API Client version #{Fulcrum::VERSION}",
+                                                    accept: 'application/json' }) do |f|
+        connection.basic_auth(opts[:username], opts[:password])
+        user = connection.get('users.json').body
+        raise ApiError, 'unknown user' if user.empty?
+        opts[:key] = user['api_token']
+      end
+      
+      raise ApiError, 'no api key' unless opts[:key]
+      
+      @connection = Faraday.new(@@uri,
+                                headers: { 'X-ApiToken' => opts[key],
+                                           user_agent: "Ruby Fulcrum API Client version #{Fulcrum::VERSION}",
+                                           accept: 'application/json' }) do |f|
         f.adapter :net_http
         f.request :multipart
         f.request :url_encoded
@@ -34,24 +36,6 @@ module Fulcrum
         f.response :json, :content_type => /\bjson$/
         f.response :mashify
       end
-      @@connection
-    end
-  end
-  
-  class Form < Api
-    
-    def self.retrieve(id)
-      connection.get("forms/#{id}").body
-    end
-    
-    def self.all(opts = {})
-      params = {}.tap do |p|
-        p[:page] = opts.delete(:page) if opts[:page]
-        p[:schema] = opts.delete(:schema) if opts[:schema]
-        p[:per_page] = opts.delete(:per_page) if opts[:per_page]
-      end
-      
-      connection.get("forms", params).body
     end
   end
 end
