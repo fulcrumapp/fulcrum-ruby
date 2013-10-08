299f3b20d52877973462d23103ae2d4a396db03e
Kyle Tolle <kyle@nullsix.com>
2013-10-08 15:44:44 -0600

    Refactor API specs for readability

--- spec/lib/api_spec.rb
+++ spec/lib/api_spec.rb
@@ -10,30 +10,28 @@ describe Fulcrum::Api do
     end
   end
 
-  it 'should be configured' do
+  it 'is configured' do
     Fulcrum::Api.configuration.uri.should eq(@uri)
     Fulcrum::Api.configuration.key.should eq(@key)
   end
 
-  it 'should get the key' do
+  it 'gets the key' do
     user = "foo@bar.com"
     pass = "foobar"
     stub_request(:get, /.*\/users.json/).to_return(:status => 200, :body => '{ "user": { "api_token": "foobar" }}')
     Fulcrum::Api.get_key(user, pass).should eq('foobar')
   end
 
-  it 'parse_opts should return only expected opts' do
+  specify '#parse_opts returns only expected opts' do
     params = Fulcrum::Api.parse_opts([:foo], { foo: 'bar', bar: 'foo'})
-    params.has_key?(:foo).should be(true)
-    params[:foo].should eq('bar')
-    params.has_key?(:bar).should be(false)
+    params.should eq({ foo: 'bar' })
   end
 
-  it 'call should raise ArgumentError with invalid method' do
+  specify '#call should raise ArgumentError with invalid method' do
     expect { Fulcrum::Api.call(:foo) }.to raise_error(ArgumentError)
   end
 
-  it 'call should not raise ArgumentError with valid method' do
+  specify '#call should not raise ArgumentError with valid method' do
     expect { Fulcrum::Api.call(:get) }.to_not raise_error(ArgumentError)
   end
 end
