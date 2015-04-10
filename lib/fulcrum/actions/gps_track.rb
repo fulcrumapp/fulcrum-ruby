81652003de2e1322154807f257bbc40203ec8019
Kyle Tolle <kyle@nullsix.com>
2015-04-10 14:36:14 -0600

    Add format parameter to #track
    For #11

--- lib/fulcrum/actions/gps_track.rb
+++ lib/fulcrum/actions/gps_track.rb
@@ -2,8 +2,8 @@ module Fulcrum
   module GpsTrack
     extend ActiveSupport::Concern
 
-    def track(id)
-      call(:get, member_action(id, 'track'))
+    def track(id, format='json')
+      call(:get, member_action(id, 'track', format))
     end
   end
 end
