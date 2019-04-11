9fd20a7c6e2aaedb9c108b0842752e3a0225fbe5
Jason Sanford <jasonsanford@gmail.com>
2019-04-11 16:25:45 -0400

    Test get_user

--- spec/lib/client_spec.rb
+++ spec/lib/client_spec.rb
@@ -21,4 +21,19 @@ describe Fulcrum::Client do
 
     expect(csv).to eq(response)
   end
+
+  it 'cat get_user' do
+    url = "https://fred%40flinstones.com:secret@api.fulcrumapp.com/api/v2/users.json"
+    response = '{"user":{"first_name":"Jason","last_name":"Sanford","email":"jason@fulcrumapp.com","image_small":"https://fulcrumapp-dev.s3.amazonaws.com/user-images/small_abc.jpg","image_large":"https://fulcrumapp-dev.s3.amazonaws.com/user-images/large_abc.jpg","id":"49d06ce5-1457-476c-9cb4-fd9d41ea43ef","contexts":[{"name":"Team Jason","id":"e09c1a03-819d-47d6-aebc-f8712d292b57","api_token":"abc123","image_small":"https://fulcrumapp-dev.s3.amazonaws.com/organization-images/small_abc.jpg","image_large":"https://fulcrumapp-dev.s3.amazonaws.com/organization-images/large_abc.jpg","type":"organization","role":{},"domain":null,"plan":{}}],"access":{"allowed":true}}}'
+
+    stub_request(:get, url)
+      .to_return(status: 200, body: response,
+                 headers: {"Content-Type" => "application/json"})
+
+    sql = 'select name from tables limit 1;'
+    user = Fulcrum::Client.get_user('fred@flinstones.com', 'secret')
+
+    expect(user['contexts'].length).to eq(1)
+    expect(user['contexts'][0]['id']).to eq('e09c1a03-819d-47d6-aebc-f8712d292b57')
+  end
 end
