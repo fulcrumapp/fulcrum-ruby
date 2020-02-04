dc34f5bbf68c386000c01ab740939e34d59ee3c6
Davey <davidmcnight@gmail.com>
2020-02-04 10:02:10 -0500

    add forms test

--- lib/fulcrum/form.rb
+++ lib/fulcrum/form.rb
@@ -5,5 +5,12 @@ module Fulcrum
     include Actions::Create
     include Actions::Update
     include Actions::Delete
+
+    def history(id)
+      result = call(:get, member_action(id, 'history'))
+
+      Page.new(result, resources_name)
+    end
+
   end
 end
--- spec/lib/form_spec.rb
+++ spec/lib/form_spec.rb
@@ -13,4 +13,24 @@ describe Fulcrum::Form do
   include_examples 'creates resource'
   include_examples 'updates resource'
   include_examples 'deletes resource'
+
+  it 'lists all resources' do
+    url = "#{client.url}/forms/8ae9115-0430-459e-a1b7-7ac46011e0ce/history.json"
+
+    stub_request(:get, url)
+      .to_return(status: 200, body: list_response,
+                 headers: {"Content-Type" => "application/json"})
+
+    page = resource.history('8ae9115-0430-459e-a1b7-7ac46011e0ce')
+
+    expect(client.response.status).to eq(200)
+
+    expect(page).to respond_to(:current_page)
+    expect(page).to respond_to(:total_pages)
+    expect(page).to respond_to(:total_count)
+    expect(page).to respond_to(:per_page)
+
+    expect(page.objects).to be_a(Array)
+  end
+
 end
